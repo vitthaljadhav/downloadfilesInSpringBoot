79b6c7f1a2b9085a1cfd813cf49b62519a373379
vitthal <jadhav.vitthal25>
2021-12-11 21:26:59 +0530

    [-] files download

---
+++ spring-boot-file-upload-download-rest-api-example-mysql-database-storage/src/main/java/com/example/filedemo/controller/FileController.java
@@ -0,0 +1,165 @@
+package com.example.filedemo.controller;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.filedemo.model.DBFile;
+import com.example.filedemo.payload.UploadFileResponse;
+import com.example.filedemo.service.DBFileStorageService;
+
+@RestController
+public class FileController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    private DBFileStorageService dbFileStorageService;
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        DBFile dbFile = dbFileStorageService.storeFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(dbFile.getId())
+                .toUriString();
+
+        return new UploadFileResponse(dbFile.getFileName(), fileDownloadUri,
+                file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.asList(files)
+                .stream()
+                .map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{fileId}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) {
+        // Load file from database
+        DBFile dbFile = dbFileStorageService.getFile(fileId);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(dbFile.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + dbFile.getFileName() + "\"")
+                .body(new ByteArrayResource(dbFile.getData()));
+    }
+    
+	//private static final String EXTERNAL_FILE_PATH = "C:/fileDownloadExample/";
+    private static final String EXTERNAL_FILE_PATH = "D:/Books/test/";
+//for Single File download
+	@RequestMapping("/file/{fileName:.+}")
+	public void downloadPDFResource(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable("fileName") String fileName) throws IOException {
+
+		File file = new File(EXTERNAL_FILE_PATH + fileName);
+		if (file.exists()) {
+
+			//get the mimetype
+			String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+			if (mimeType == null) {
+				//unknown mimetype so set the mimetype to application/octet-stream
+				mimeType = "application/octet-stream";
+			}
+
+			response.setContentType(mimeType);
+
+			/**
+			 * In a regular HTTP response, the Content-Disposition response header is a
+			 * header indicating if the content is expected to be displayed inline in the
+			 * browser, that is, as a Web page or as part of a Web page, or as an
+			 * attachment, that is downloaded and saved locally.
+			 * 
+			 */
+
+			/**
+			 * Here we have mentioned it to show inline
+			 */
+			response.setHeader("Content-Disposition", String.format("anything; filename=\"" + file.getName() + "\""));
+
+			 //Here we have mentioned it to show as attachment
+			 //response.setHeader("Content-Disposition", String.format("attachment; filename=\"" + file.getName() + "\""));
+
+			response.setContentLength((int) file.length());
+
+			InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+			FileCopyUtils.copy(inputStream, response.getOutputStream());
+
+		}
+	}
+
+	
+	 @GetMapping("/downloadZip/{listOfFileName}")
+	    public void downloadFile(HttpServletResponse response , @PathVariable("listOfFileName") String listOfFileName) {
+
+	        response.setContentType("application/octet-stream");
+	        response.setHeader("Content-Disposition", "attachment;filename=irctc-doc.zip");
+	        response.setStatus(HttpServletResponse.SC_OK);
+
+	        List<String> fileNames = new ArrayList<String>();
+	        String[] fileSplit = listOfFileName.split(",");
+	        for(String file :fileSplit ) {
+	        	fileNames.add(EXTERNAL_FILE_PATH+file);
+	        }
+	        //fileNames.add(EXTERNAL_FILE_PATH+"test.pdf");
+	       // fileNames.add(EXTERNAL_FILE_PATH+"best.pdf");
+
+	        System.out.println("file size -" + fileNames.size());
+
+	        try (ZipOutputStream zippedOut = new ZipOutputStream(response.getOutputStream())) {
+	            for (String file : fileNames) {
+	                FileSystemResource resource = new FileSystemResource(file);
+
+	                ZipEntry e = new ZipEntry(resource.getFilename());
+	                // Configure the zip entry, the properties of the file
+	                e.setSize(resource.contentLength());
+	                e.setTime(System.currentTimeMillis());
+	                // etc.
+	                zippedOut.putNextEntry(e);
+	                // And the content of the resource:
+	                StreamUtils.copy(resource.getInputStream(), zippedOut);
+	                zippedOut.closeEntry();
+	            }
+	            zippedOut.finish();
+	        } catch (Exception e) {
+	            // Exception handling goes here
+	        }
+	    }
+
+}
